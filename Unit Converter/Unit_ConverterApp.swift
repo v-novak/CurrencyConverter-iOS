79873185892a43f516a4863c7135d1b2aa0f31ce
Vasiliy Novak (Sabadazh) <vnovak2048@gmail.com>
2023-01-04 18:57:49 +0400

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Vasiliy Sabadazh on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
